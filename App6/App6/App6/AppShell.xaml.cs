3431db7082b4e3565c73b509d266c48841658bb2
zacharskapaula <84339270+zacharskapaula@users.noreply.github.com>
2022-04-07 15:48:17 +0200

    visual files

---
+++ App6/App6/App6/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using App6.ViewModels;
+using App6.Views;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ App6/App6/App6/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using App6.Models;
+using App6.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
