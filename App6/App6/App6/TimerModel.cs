28fca4339146c6fdf9ebd862bc3c0646cd2b3fa0
zacharskapaula <84339270+zacharskapaula@users.noreply.github.com>
2022-04-21 13:12:52 +0200

    Add Timer
    add timer, delete pins

---
+++ App6/App6/App6/TimerModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+using System.Timers;
+
+namespace App6
+{
+    public class TimerModel : INotifyPropertyChanged
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        private Timer time = new Timer();
+        //private bool timerRunning;
+        private string _stopWatchHours;
+
+        public string StopWatchHours
+        {
+            get { return _stopWatchHours; }
+            set
+            {
+                _stopWatchHours = value;
+                OnPropertyChanged("StopWatchHours");
+            }
+        }
+
+        private string _stopWatchMinutes;
+        public string StopWatchMinutes
+        {
+            get { return _stopWatchMinutes; }
+            set
+            {
+                _stopWatchMinutes = value;
+                OnPropertyChanged("StopWatchMinutes");
+            }
+        }
+
+        private string _stopWatchSeconds;
+        public string StopWatchSeconds
+        {
+            get { return _stopWatchSeconds; }
+            set
+            {
+                _stopWatchSeconds = value;
+                OnPropertyChanged("StopWatchSeconds");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if(changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public TimerModel()
+        {
+            stopwatch.Start();
+            StopWatchHours = stopwatch.Elapsed.Hours.ToString();
+            StopWatchMinutes = stopwatch.Elapsed.Minutes.ToString();
+            StopWatchSeconds = stopwatch.Elapsed.Seconds.ToString();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                StopWatchHours = stopwatch.Elapsed.Hours.ToString();
+                StopWatchMinutes = stopwatch.Elapsed.Minutes.ToString();
+                StopWatchSeconds = stopwatch.Elapsed.Seconds.ToString();
+                return true;
+
+            });
+        }
+    }
+}
