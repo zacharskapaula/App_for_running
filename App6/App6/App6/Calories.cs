999e99968aa2c45985fc75ca9c8512be643eff75
zacharskapaula <84339270+zacharskapaula@users.noreply.github.com>
2022-05-02 15:13:00 +0200

    Add calories class

---
+++ App6/App6/App6/Calories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App6
+{
+    class Calories
+    {
+        // jeśli przebiegmnie 25 metrów to 1,55 kcal zostanie dodane do spalonych kalorii
+    }
+}
